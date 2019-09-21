601401c55cd89bc8b9c3b81666fe2d298ffe2acf
Jacob <jackeaik@hotmail.com>
2019-09-21 20:09:40 +0200

    Feat: Two Sum Completed

---
+++ Visual-studio-solutions/Two-sum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Visual_studio_solutions
+{
+    class Two_sum
+    {
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var currentNumber = nums[i];
+                var complement = target - currentNumber;
+                // Check if we have seen the complement before and that the indices are different for an acceptable solution
+                if (dict.ContainsKey(complement) && dict[complement] != i)
+                {
+                    return new int[] { dict[complement], i };
+                }
+                    
+                // Save the current index at position currentNumber
+                dict[currentNumber] = i;
+            }
+            throw new ArgumentException("No solution found.");
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+
+            int[] arr = { 1, 2, 3 };
+            var target = 9;
+            int[] result = TwoSum(arr, target);
+
+            Console.WriteLine(result[0] + " " + result[1]);
+
+        }
+
+    }
+}
